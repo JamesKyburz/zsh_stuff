9da87141f6dd4e773c4ae7c7c2849d710050cf33
James Kyburz <james.kyburz@gmail.com>
2016-01-03 16:50:41 +0100

    Added merge

--- greenkeeper.zsh
+++ greenkeeper.zsh
@@ -7,9 +7,29 @@ clear-greenkeeper-branches() {
 
 clear-all-greenkeeper-stuff() {
   cd ~/Documents/src
-  for repo in $(greenkeeper list | sed 's/JamesKyburz\///g'); do
+  user=$(git config --get user.username)
+  for repo in $(greenkeeper list | sed 's/$user\///g'); do
     cd $repo
     gl
     clear-greenkeeper-branches
   done
+}
+
+merge-greenkeeper() {
+  remotebranch=$(git branch --all | grep greenkeeper | head -n1 | sed 's/[ ]//g')
+  localbranch=$(echo $remotebranch | cut -d "/" -f3)
+  if [ "$remotebranch" = "" ]; then
+    echo "no greenkeeper branch found"
+    return
+  fi
+  git co -b $localbranch $remotebranch
+  git diff master
+  read merge\?"merge $localbranch? "
+  if [ "$merge" = "y" ]; then
+    git rebase master
+    git co master
+    git merge --no-ff $localbranch
+  else
+    git co master
+  fi
 }
