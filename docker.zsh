c9133f35ffe2755c782bd06e4e494929ef73ea16
James Kyburz <james.kyburz@gmail.com>
2016-04-21 17:00:41 +0200

    docker-ssh

--- docker.zsh
+++ docker.zsh
@@ -10,3 +10,7 @@ docker-env() {
   eval $(docker-machine env $1 --shell=bash 2&>/dev/null| grep export)
   export PS1="docker $1 $__PS1"
 }
+
+docker-ssh() {
+  ssh root@$(docker-machine ip $1) -i ~/.docker/machine/machines/$1/id_rsa
+}
