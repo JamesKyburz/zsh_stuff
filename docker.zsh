3ea5a620f7fced671afc3d98459b5c3e71030ae7
James Kyburz <james.kyburz@gmail.com>
2016-05-02 15:49:30 +0200

    docker-env fix when switching docker versions

--- docker.zsh
+++ docker.zsh
@@ -7,8 +7,10 @@ docker-default() {
 }
 
 docker-env() {
-  eval $(docker-machine env $1 --shell=bash 2&>/dev/null| grep export)
-  export PS1="docker $1 $__PS1"
+  export DOCKER_CERT_PATH=~/.docker/machine/machines/$1
+  export DOCKER_HOST=tcp://$(docker-machine ip $1)
+  export DOCKER_MACHINE_NAME=$1
+  export DOCKER_TLS_VERIFY=0
 }
 
 docker-ssh() {
