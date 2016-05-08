3273e978069b49fc5eeb66a6e7e074dd7743d902
James Kyburz <james.kyburz@gmail.com>
2016-05-08 13:22:56 +0200

    docker-env fixes + prompt

--- docker.zsh
+++ docker.zsh
@@ -8,9 +8,10 @@ docker-default() {
 
 docker-env() {
   export DOCKER_CERT_PATH=~/.docker/machine/machines/$1
-  export DOCKER_HOST=tcp://$(docker-machine ip $1)
+  export DOCKER_HOST=$(docker-machine url $1)
   export DOCKER_MACHINE_NAME=$1
   export DOCKER_TLS_VERIFY=0
+  export PS1="docker $1 $__PS1"
 }
 
 docker-ssh() {
